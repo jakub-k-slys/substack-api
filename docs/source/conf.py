4d094d7f5f00fa647bed79b159a7fbdfdc6b122d
Jakub Slys <jakub@slys.dev>
2025-06-21 23:43:34 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,59 +1,26 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Substack API'
 copyright = '2023, Jakub Slys'
 author = 'Jakub Slys'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+release = '0.1'
+version = '0.1.0'
 
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'myst_parser',
-    'myst_parser.sphinx_'
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
 
-# MyST configuration
-myst_enable_extensions = [
-    "colon_fence",
-    "deflist",
-    "dollarmath",
-    "fieldlist",
-    "html_admonition",
-    "html_image",
-    "replacements",
-    "smartquotes",
-    "substitution",
-    "tasklist"
-]
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = []
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-html_static_path = ['_static']
 
-# -- Extension configuration -------------------------------------------------
-# Source file parsers
-source_suffix = {
-    '.md': 'markdown',
-}
-
-# Default source file type
-source_parsers = {
-    '.md': 'markdown',
-}
-
-# Default source file extension
-source_suffix = '.md'
+epub_show_urls = 'footnote'
